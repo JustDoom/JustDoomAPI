578667151b3cf009f157cee0db970bc008203b54
JustDoom <61824552+JustDoom@users.noreply.github.com>
2022-11-02 18:56:58 +1000

    Update AuthController.java

--- src/main/java/com/imjustdoom/justdoomapi/controller/AuthController.java
+++ src/main/java/com/imjustdoom/justdoomapi/controller/AuthController.java
@@ -1,20 +1,12 @@
 package com.imjustdoom.justdoomapi.controller;
 
-import com.imjustdoom.justdoomapi.dto.in.BlogPostDto;
+import com.imjustdoom.justdoomapi.config.APIConfig;
 import com.imjustdoom.justdoomapi.dto.in.LoginDto;
 import com.imjustdoom.justdoomapi.dto.in.RegisterDto;
-import com.imjustdoom.justdoomapi.model.Account;
-import com.imjustdoom.justdoomapi.model.BlogPost;
-import com.imjustdoom.justdoomapi.repository.AccountRepository;
-import com.imjustdoom.justdoomapi.repository.BlogRepository;
-import com.imjustdoom.justdoomapi.repository.ProjectRepository;
-import com.imjustdoom.justdoomapi.repository.TokenRepository;
-import com.imjustdoom.justdoomapi.service.AccountService;
-import com.imjustdoom.justdoomapi.util.APIUtil;
+import com.imjustdoom.justdoomapi.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,21 +16,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AccountService accountService;
+    private final AuthService authService;
+
+    private final APIConfig config;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto data, @CookieValue(name = "token", required = false) String token) {
-        return accountService.login(data, token);
+        return authService.login(data, token);
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterDto data, @CookieValue(name = "token", required = false) String token) {
-        return accountService.register(data, token);
+        return authService.register(data, token);
     }
 
     @PostMapping("/logout2")
     public ResponseEntity<?> logout() {
-        return accountService.logout();
+        return authService.logout();
     }
 
     @Bean
@@ -47,8 +41,8 @@ public class AuthController {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/auth/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOriginPatterns("http://localhost:3000")
+                        .allowedOrigins(config.getFrontendUrl())
+                        .allowedOriginPatterns(config.getFrontendUrl())
                         .allowCredentials(true)
                         .allowedMethods("GET", "POST");
             }
