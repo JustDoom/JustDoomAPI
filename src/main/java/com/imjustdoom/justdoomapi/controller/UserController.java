7d53057226f844595e1b963e202d27b2cc2b91e5
JustDoom <61824552+JustDoom@users.noreply.github.com>
2022-11-20 21:25:13 +1000

    start user info thing

--- src/main/java/com/imjustdoom/justdoomapi/controller/UserController.java
+++ src/main/java/com/imjustdoom/justdoomapi/controller/UserController.java
@@ -2,6 +2,7 @@ package com.imjustdoom.justdoomapi.controller;
 
 import com.imjustdoom.justdoomapi.dto.in.UserFromTokenDto;
 import com.imjustdoom.justdoomapi.dto.out.SimpleUserDto;
+import com.imjustdoom.justdoomapi.dto.out.UserDto;
 import com.imjustdoom.justdoomapi.repository.AccountRepository;
 import com.imjustdoom.justdoomapi.service.AuthService;
 import com.imjustdoom.justdoomapi.util.APIUtil;
@@ -37,6 +38,18 @@ public class UserController {
         }
 
         return ResponseEntity.ok().body(accountRepository.findAll().stream().map(user -> SimpleUserDto.create(user.getUsername(), user.getRole(), user.getJoined().toEpochSecond(ZoneOffset.ofHours(10)), user.getId())).collect(Collectors.toList()));
+        //return accountRepository.findAll().isEmpty() ? ResponseEntity.ok().body("No users found") : ResponseEntity.ok().body(accountRepository.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserInfo(@RequestHeader("authorization") String token, @PathVariable int id) {
+
+        if(!authService.isAdmin(token)) {
+            return ResponseEntity.ok().body(APIUtil.createErrorResponse("You are not an admin"));
+        }
+
+        return ResponseEntity.ok().body(accountRepository.findById(id).map(user -> UserDto.create(user.getUsername(), user.getRole(), user.getJoined().toEpochSecond(ZoneOffset.ofHours(10)), user.getId())).orElse(null));
+
         //return accountRepository.findAll().isEmpty() ? ResponseEntity.ok().body("No users found") : ResponseEntity.ok().body(accountRepository.findAll());
     }
 
